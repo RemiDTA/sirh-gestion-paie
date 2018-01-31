c7b4c550fbd5ea8cf719012a35ca919922312ce4
Remi <ferreira-remi@hotmail.fr>
2018-01-31 17:01:49 +0100

    tp 2 partie 1

--- src/main/java/dev/paie/service/CalculerRemunerationServiceSimpleTest.java
+++ src/main/java/dev/paie/service/CalculerRemunerationServiceSimpleTest.java
@@ -4,11 +4,15 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import dev.paie.config.ServicesConfig;
 import dev.paie.entite.ResultatCalculRemuneration;
 
-//Sélection des classes de configuration Spring à utiliser lors du test
+// Sélection des classes de configuration Spring à utiliser lors du test
 @ContextConfiguration(classes = { ServicesConfig.class })
 // Configuration JUnit pour que Spring prenne la main sur le cycle de vie du
 // test
